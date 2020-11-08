eb60e0f79373f8287cdc4fc5557ea3d1b090ac8d
susannalankocz1999 <susanna.lankocz@logima.de>
2020-11-08 08:57:25 +0100

    work in progress Angular

--- mrsGo-frontend/src/app/app-routing.module.ts
+++ mrsGo-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ChatComponent} from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mrsGo-frontend/src/app/app.module.ts
+++ mrsGo-frontend/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChatComponent } from './chat/chat.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ChatComponent,
-    ContactsComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
